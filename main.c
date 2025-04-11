084d76005f947166d7bd93e5a5ce45c6d2cf31a7
Marcos Sousa <sousas1569@gmail.com>
2025-04-11 17:07:01 -0300

    criacao de pastas para organizacao

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "agenda.h"
+#include "src/Agenda/agenda.h"
 
 int main()
 {
--- src/Agenda/agenda.c
+++ src/Agenda/agenda.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "listas.h"
+#include "../listas.h"
 #include "agenda.h"
 
 // Funcao para imprimir todos os elementos da lista
